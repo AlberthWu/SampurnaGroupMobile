c94e097ebfbcde10ea521f4c1d26f236f98cd47c
Dwi Rizki Oktaviani <dwirizkiokta@gmail.com>
2023-01-07 11:51:29 +0700

    Update Pallete

---
+++ lib/pallete.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+class Pallete {
+    static const Color iconColor = Color.fromARGB(214, 166, 196, 235);
+    static const Color activeColor = Color.fromARGB(255, 6, 35, 73);
+    static const Color textColor1 = Color.fromARGB(204, 181, 182, 182);
+    static const Color textColor2 = Color.fromARGB(246, 82, 85, 90);
+    static const Color facebookColor = Color.fromARGB(255, 59, 89, 153);
+    static const Color googleColor = Color.fromARGB(213, 222, 75, 57);
+    static const Color backgroundColor = Color.fromARGB(214, 166, 196, 235);
+}
