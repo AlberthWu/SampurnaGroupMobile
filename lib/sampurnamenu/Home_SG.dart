005a9bede4acc8a3ea0c6ab1d92ec62c288a552f
Dwi Rizki Oktaviani <dwirizkiokta@gmail.com>
2023-03-02 09:06:54 +0700

    Update Button

---
+++ lib/sampurnamenu/Home_SG.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:sampurnagroupmobile/constants.dart';
+import 'package:sampurnagroupmobile/menu.dart';
+import 'package:sampurnagroupmobile/sampurnamenu/saldocuti.dart';
+
+class HomeSG extends StatelessWidget {
+  const HomeSG({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+          backgroundColor: Constants.primaryColor,
+          elevation: 0,
+          toolbarHeight: 71,
+      ),
+      body: SingleChildScrollView(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [Menu(), SaldoCuti()],
+      )),
+    );
+  }
+}
