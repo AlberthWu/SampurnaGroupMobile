980aef318456ea5701c504b78e550a5156dd0724
Dwi Rizki Oktaviani <dwirizkiokta@gmail.com>
2023-03-13 16:04:11 +0700

    Update List Riwayat

--- lib/Final_Menu/Models/history_models.dart
+++ lib/Final_Menu/Models/history_models.dart
@@ -1,25 +1,119 @@
-class HistoryModel {
-  final String rideId;
-  final String source;
-  final String destination;
-  final String unit;
-  final String noPol;
-  final String grupPT;
-  final int noSj;
-  final String noSc;
-  final String time;
-  
+import 'package:flutter/material.dart';
+import 'package:sampurnagroupmobile/constants.dart';
 
-  HistoryModel({
-    required this.rideId,
-    required this.source,
-    required this.destination,
-    required this.unit,
-    required this.noPol,
-    required this.grupPT,
-    required this.noSj,
-    required this.noSc,
-    required this.time,
-  });
+class history1 extends StatefulWidget {
+  const history1({Key? key}) : super(key: key);
 
+  @override
+  _history1State createState() => _history1State();
+}
+
+class _history1State extends State<history1> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.fromLTRB(20, 25, 0, 10),
+            child: const Text(
+              "Riwayat",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ),
+          Flexible(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFEEEEEE),
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(top: 5),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    const Text(
+                                      "B 1572 HJ",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    const Text(
+                                      "To Rumpin",
+                                      style: TextStyle(
+                                        // fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Text("08 agustus 2023"),
+                                  const Text(
+                                    "Rp 1.000.000",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              ElevatedButton(
+                                child: Text(
+                                  'Rincian',
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(35)),
+                                    backgroundColor: Constants.primaryColor,
+                                    textStyle: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12,
+                                      fontStyle: FontStyle.normal
+                                    ), 
+                                  ),
+                                  onPressed: () {},
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
